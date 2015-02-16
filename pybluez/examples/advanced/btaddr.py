8f8d16eabe9a51951fe78f3fe141758cb970964b
SunKyu <leesunkeuy@naver.com>
2015-02-16 06:57:41 +0000

    make btaddr class

---
+++ pybluez/examples/advanced/btaddr.py
@@ -0,0 +1,15 @@
+class BTaddr():
+  addr = " "
+  rssi = " "
+  def setaddr(in_addr):
+    self.addr = in_addr
+  
+  def setrssi(in_rssi):
+    self.rssi = in_rssi
+  
+  def getaddr():
+    return self.addr
+  
+  def getrssi():
+    return self.rssi
+
