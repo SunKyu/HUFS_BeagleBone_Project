07bbea5a5b1641dce8c9b31207395821b1ce15bb
SunKyu Lee <leesunkeuy@naver.com>
2015-02-21 17:05:45 +0900

    make client module

---
+++ pybluez/examples/test/clientmodule.py
@@ -0,0 +1,25 @@
+# file: l2capclient.py
+# desc: Demo L2CAP client for bluetooth module.
+# $Id: l2capclient.py 524 2007-08-15 04:04:52Z albert $
+
+import sys
+import bluetooth
+def clinetmodule(message, addr):
+  if sys.version < '3':
+    input = raw_input
+
+  sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+  bt_addr=addr
+  port = 0x1001
+
+  print("trying to connect to %s on PSM 0x%X" % (bt_addr, port))
+
+  sock.connect((bt_addr, port))
+
+  print("connected.  type stuff")
+  sock.send(message)
+  response = sock.recv(1024)
+  print(response)
+  sock.close()
+  return response
+
