bd72b5e6f55ab787efe2458bf55c71ec5c80cd02
SunKyu Lee <leesunkeuy@naver.com>
2015-03-01 17:48:55 +0900

    add auto bluetooth install

--- init.sh
+++ init.sh
@@ -5,8 +5,15 @@ sudo apt-get install -y python-pip
 sudo apt-get install -y python-smbus
 sudo apt-get install -y bluez
 sudo apt-get install -y libbluetooth-dev
+sudo apt-get install -y unzip
 sudo pip install Adafruit_BBIO
 sudo pip install pybluez
+sudo cd /opt
+sudo wget --no-check-certificate https://pybluez.googlecode.com/files/PyBluez-0.20.zip
+sudo unzip PyBluez-0.20.zip
+sudo cd PyBluez-0.20
+sudo python setup.py install
+
 
 #cd /opt
 #git clone https://github.com/SunKyu/Vim_vimrc.git
