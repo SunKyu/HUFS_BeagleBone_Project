c28f4ccc7282851c73a0b030884e375e7d84941a
SunKyu Lee <leesunkeuy@naver.com>
2015-03-03 15:02:40 +0900

    newskeloton

---
+++ pybluez/examples/test/new.py
@@ -0,0 +1,16 @@
+from test_inquiry import *
+import bluetooth
+from clientmodule import *
+from servermoudle import *
+import sys
+addr = getaddr_rssi()
+dic_addr ={}
+parent = []
+child = [] 
+number = -1
+count = -1
+
+while 1: 
+  
+  
+
