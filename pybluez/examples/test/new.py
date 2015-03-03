f7ccc80e93a325cd872dc0f92c7ab6dc22d83184
SunKyu Lee <leesunkeuy@naver.com>
2015-03-03 19:34:19 +0900

    complete the getres function

--- pybluez/examples/test/new.py
+++ pybluez/examples/test/new.py
@@ -3,16 +3,17 @@ import bluetooth
 from clientmodule import *
 from servermoudle import *
 import sys
-addr = getaddr_rssi()
-dic_addr ={}
-parent = []
-child = [] 
+addr = getaddr_rssi() #list of address and rssi 
+dic_addr ={} #address dictionary
+parent = [] #parent
+child = [] #childe list
 number = -1
 count = -1
-indexflag   = 0
+indexflag  = 0 #need to change when all child visit (get, search)
+get_target = -1 #store targetnumber
+sensor_type = ""#store sensor_type
+light_state = 0 #store light state
 
-
-light_state = 0
 
 def search(dataparse, address):
  if len(parent) is not 0:
@@ -32,8 +33,11 @@ def search(dataparse, address):
 def searchres(dataparse, address):
 
 
+
 #----end-----
 def get(dataparse, address):
+  get_target = int(dataparse[1])
+  sensor_type = dataparse[2]
   if int(dataparse[1]) is number:
     #send success message
     message = "%s/%s/%d" %("getres","success" ,light_state)
@@ -42,18 +46,18 @@ def get(dataparse, address):
     else:
       #do we need a case??
 
+  elif len(child) is 0: 
+    #if this node is leaf node and incorrect number node
+    #send fail message to parent 
+    message = "%s/%s/%d" %("getres", "fail", -1)
+    clientmodule(message, dic_addr[parent[0]])
+
   elif int(dataparse[1]) in child:
     #if the target in child array
     child_index = child.index(int(dataparse[1]))
     message = "%s/%s/%s" %(dataparse[0], dataparse[1], dataparse[2])
     clientmodule(message, dic_addr[child[child_index]])
   
-  elif len(child) is 0: 
-    #if this node is leaf node and incorrect number node
-    #send fail message to parent 
-    message = "%s/%s/%d" %("searchres", "fail", -1)
-    clientmodule(message, dic_addr[parent[0]])
-
   else:
     #check the index flag
     #and then send message
@@ -68,6 +72,22 @@ def get(dataparse, address):
 
 #----end-----
 def getres(dataparse, address):
+  if dataparse[1] is "success":
+    #if data is success
+    message = "%s/%s/%s" %(dataparse[0], dataparse[1], dataparse[2])
+    clientmodule(message, dic_addr[parent[0]])
+  else:
+    #if data is fail
+    if indexflag < len(child):
+      #send get message other childs
+      message = "%s/%d/%s" %("get",get_target , sensor_type) 
+      clientmodule(message, dic_addr[child[indexflag]])
+      indexflag = indexflag+1
+
+    else:
+      #send fail(getres) message to parent, because already check all child
+      message = "%s/%s/%s" %(dataparse[0], dataparse[1], dataparse[2])
+      clientmodule(message, dic_addr[parent[0]])
 
 
 #----end-----
